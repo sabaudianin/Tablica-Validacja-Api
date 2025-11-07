2c50e9d87217ac7660cafb2b97e632bf2076a15b
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 <sabaudianin@gmail.com>
2025-11-07 20:05:48 +0100

    editUser Form import

--- src/pages/apiTask/ApiTask.tsx
+++ src/pages/apiTask/ApiTask.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import type { User } from "../../types/types";
 import { useGetUser } from "../../hooks/useGetUser";
 import { useEditUser } from "../../hooks/useEditUser";
-
 import { EditUserForm } from "../../components/EditUserForm/EditUserForm";
 
 export const ApiTask: React.FC = () => {
