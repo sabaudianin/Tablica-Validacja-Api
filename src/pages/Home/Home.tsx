96599470816014a82a102022a4c88aeea83e297a
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 <sabaudianin@gmail.com>
2025-11-10 14:38:31 +0100

    feat Home tests

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router";
+
 export const Home = () => {
   return (
     <div className="grid gap-2 grid-rows-[auto_1fr_auto]  w-full">
@@ -13,17 +15,35 @@ export const Home = () => {
       <div className="mx-auto gap-2  w-full grid  grid-cols-1 md:grid-cols-[120px_minmax(0,1fr)_120px] rounded-3xl">
         <div className="hidden md:block  rounded-3xl min-h-[50vh] static-sides"></div>
 
-        <div className="bg-blue-400/10 flex justify-center items-center font-bold text-xl rounded-3xl p-4 min-h-[50vh] static-background">
-          Rafał Bobko
+        <div className="bg-blue-400/10 flex flex-col justify-center gap-8 items-center font-bold text-xl rounded-3xl p-4 min-h-[50vh] static-background">
+          <h2> Spis Tresci:</h2>
+          <div className="text-xs">
+            <p>
+              <NavLink to="/tekst">LITEROPRZESTAWIACZ</NavLink>- losowo
+              przestawia szyk liter w każdym wyrazie, oprócz pierwszej i
+              ostatniej litery Pozwala na wpisanie dowolnego tekstu za pomocą
+              formularza lub załadowanie pliku tekstowego.
+            </p>
+            <p className="my-4 md:my-16">
+              <NavLink to="/pesel">PESELCHECKER </NavLink>- Aplikacja validująca
+              poprawność numeru PESEL, po podaniu prawidłowego PESELU podaje
+              datę urodzenia i płeć. Aplikacja przetestowana testami
+              jednostkowymi przy pomocy VITEST.
+            </p>
+            <p>
+              <NavLink to="/api">API CRUD</NavLink> - Implementacja CRUD w
+              kontekście komunikacji z zewnętrznym API
+            </p>
+          </div>
         </div>
 
-        <div className=" hidden md:block   rounded-3xl static-sides"></div>
+        <div className=" hidden md:block rounded-3xl static-sides"></div>
       </div>
       <a
-        href="mailto:rafbobbob@vp.pl"
+        href="mailto:rafbobbob@gmail.com"
         className="col-span-full bg-white/20 p-4 font-bold rounded-3xl transition hover:underline "
       >
-        rafbobbob@vp.pl
+        rafbobbob@gmail.com
       </a>
     </div>
   );
